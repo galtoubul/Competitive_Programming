244a5358c7598a91135dd875fa86420c081fb9bd
galtoubul <galtoubul@gmail.com>
2020-04-24 19:24:36 +0300

    .

---
+++ L01/Display_The_Number.cpp
@@ -0,0 +1,24 @@
+// CodeForces/Educational Codeforces Round 81/A. Display The Number
+// Created by Gal Toubul
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stdlib.h>
+#include <set>
+#include <map>
+#include <queue>
+using namespace std;
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<ll> VL;
+typedef vector<vector<int>> VVI;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cout << "hey" << endl;
+    return 0;
+}
+
+
