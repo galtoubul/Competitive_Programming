3873886e5a5c1ee9d66b7a8db570a7e548a6fc87
galtoubul <galtoubul@gmail.com>
2020-04-25 08:05:25 +0300

    .

--- L01/Display_The_Number.cpp
+++ L01/Display_The_Number.cpp
@@ -2,23 +2,30 @@
 // Created by Gal Toubul
 
 #include <iostream>
-#include <vector>
 #include <string>
-#include <algorithm>
-#include <stdlib.h>
-#include <set>
-#include <map>
-#include <queue>
 using namespace std;
 typedef long long ll;
-typedef vector<int> VI;
-typedef vector<ll> VL;
-typedef vector<vector<int>> VVI;
 
 int main(){
     ios::sync_with_stdio(0);
-    cout << "hey" << endl;
+    int t;
+    cin >> t;
+
+    while (t--){
+        int n;
+        cin >> n;
+        // the best way to get a bigger number is by adding 7 or 1 to the MSB, because they require only 2 or 3 segments
+        // and if we an odd number than it's better to place 7 at the MSB and not at the LSB
+        // printing strings instead of working with long long is mandatory since the program should with n<=10^5
+        if (n % 2 == 1) {
+            cout << "7";
+            n -= 3;
+        }
+        while (n >= 2) {
+            cout << "1";
+            n -= 2;
+        }
+        cout << endl;
+    }
     return 0;
 }
-
-
